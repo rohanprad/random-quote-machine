9eb06850d077b6566d235f0a2a195d2ca78e790b
Rohan Pradhan <rohanprad09@gmail.com>
2020-10-14 18:02:50 +0530

    Updates

---
+++ precache-manifest.ba81b9bb0f2af3fdb3284907b54a19dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b9437e4ce38b521ff737de2b264c1e1",
+    "url": "/random-quote-machine/index.html"
+  },
+  {
+    "revision": "4e9a63e0769f6940dbb6",
+    "url": "/random-quote-machine/static/css/main.f77e5fb9.chunk.css"
+  },
+  {
+    "revision": "f2bdc3b572a7ab72c6a8",
+    "url": "/random-quote-machine/static/js/2.4940843d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/random-quote-machine/static/js/2.4940843d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e9a63e0769f6940dbb6",
+    "url": "/random-quote-machine/static/js/main.c0b43b8b.chunk.js"
+  },
+  {
+    "revision": "5a57d55656c6206ad7fd",
+    "url": "/random-quote-machine/static/js/runtime-main.6db6dd27.js"
+  }
+]);
